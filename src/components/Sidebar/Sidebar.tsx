17b411a3e31ae329e6e8f5790f4ea7cd804d936d
Sergei Makhmudov <redwarbanner@gmail.com>
2025-06-27 19:40:10 +0700

    sidebar style and code refactor

---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,41 @@
+import { useLocation } from 'react-router-dom';
+import { Menu } from 'antd';
+import { BulbOutlined } from '@ant-design/icons';
+import { useTheme } from '../../hooks/useTheme.ts';
+import './Sidebar.css';
+import { getMenuItems } from './utils/getMenuItems.tsx';
+
+const Sidebar = () => {
+  const { isDark, toggleTheme } = useTheme();
+  const location = useLocation();
+  const menuItems = getMenuItems();
+
+  return (
+      <div className="sidebar">
+        <div className="top">
+          <div className="branding">
+            <BulbOutlined className="icon"/>
+            <h1>Tentry</h1>
+          </div>
+        </div>
+
+        <Menu
+            mode="inline"
+            selectedKeys={[location.pathname]}
+            items={menuItems}
+            className="menu"
+        />
+
+        <div className="theme-toggle-switch" onClick={toggleTheme} role="button" aria-label="Переключить тему">
+          <div className={`switch ${isDark ? 'dark' : 'light'}`}>
+            <div className="slider">
+              <div className="icon sun">☀️</div>
+              <div className="icon moon">🌙</div>
+            </div>
+          </div>
+        </div>
+      </div>
+  );
+};
+
+export default Sidebar;
---
+++ src/components/Sidebar/utils/getMenuItems.tsx
@@ -0,0 +1,36 @@
+import { NavLink } from 'react-router-dom';
+import {
+    DashboardOutlined,
+    FileTextOutlined,
+    CheckCircleOutlined,
+    GlobalOutlined,
+    EditOutlined
+} from '@ant-design/icons';
+
+export const getMenuItems = () => [
+    {
+        key: '/dashboard',
+        icon: <DashboardOutlined/>,
+        label: <NavLink to="/dashboard">Dashboard</NavLink>
+    },
+    {
+        key: '/analyzer',
+        icon: <FileTextOutlined />,
+        label: <NavLink to="/analyzer">Анализ текста</NavLink>
+    },
+    {
+        key: '/uniqueness',
+        icon: <CheckCircleOutlined />,
+        label: <NavLink to="/uniqueness">Уникальность</NavLink>
+    },
+    {
+        key: '/page-analyzer',
+        icon: <GlobalOutlined />,
+        label: <NavLink to="/page-analyzer">Анализ страницы</NavLink>
+    },
+    {
+        key: '/editor',
+        icon: <EditOutlined />,
+        label: <NavLink to="/editor">Редактор</NavLink>
+    }
+];
